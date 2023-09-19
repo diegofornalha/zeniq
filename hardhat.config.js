d3f74835e76a7fe016afbf11503dafd52189ba69
Diego Fornalha <diegofornalha@gmail.com>
2023-09-18 23:09:59 -0300

    config outro erro

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,57 +1,26 @@
-// Adicione esta linha no início do arquivo para carregar as variáveis de ambiente
-require('dotenv').config();
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();  
 
-require("@nomiclabs/hardhat-ethers");
-const {extendEnvironment} = require('hardhat/config');
-const fs = require("fs");
-const {resolve} = require("path");
-
-// Carregue o mnemônico do arquivo .env
-const { MNEMONIC } = process.env;
-
-let mnemonic;
-try {
-  mnemonic = fs.readFileSync(resolve(__dirname, '.secret')).toString().trim();
-} catch (ignore){
-  mnemonic = MNEMONIC;
-}
-
-const accounts = {
-    mnemonic,
-    count: 10
-}
-
-extendEnvironment(hre => {
-   hre.accounts = hre.ethers.getSigners().then(ac => ac.map(a => a.address));
-});
-
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-    solidity: {
-        compilers: [
-            {
-                version: "0.8.19",
-                settings: {
-                    optimizer: {
-                        enabled: true,
-                        runs: 200000
-                    },
-                    viaIR: true
-                }
-            }
-        ]
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      // Configuração para a rede Hardhat (local)
     },
-    networks: {
-        localhost: {
-            accounts
-        },
-        hardhat: {
-            accounts
-        },
-        zeniq: {
-            accounts,
-            url: "https://smart.zeniq.network:9545",
-            chainId: 383414847825
-        }
+    zeniq: {
+      url: "https://smart.zeniq.network:9545",  // URL do nó RPC da Zeniq
+      accounts: [`0x${process.env.PRIVATE_KEY}`],  // Usa a chave privada do arquivo .env
+      gasPrice: 20000000000,  // Preço do gás (ajuste conforme necessário)
+      chainId: 383414847825  // chainId da rede Zeniq
     }
+  },
+  solidity: {
+    version: "0.7.3", 
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
 };
